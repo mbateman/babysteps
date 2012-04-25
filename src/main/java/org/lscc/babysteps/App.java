4863e561aa630a16d0b3e43a75a4800b2539d1ac
Michael Bateman <michael.bateman.com@gmail.com>
2012-04-25 18:35:57 +0100

    first commit

---
+++ src/main/java/org/lscc/babysteps/App.java
@@ -0,0 +1,13 @@
+package org.lscc.babysteps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
